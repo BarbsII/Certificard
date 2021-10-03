31b9a386cc25f360b574b931add5b37f2f2538a5
BarbsII <91839395+BarbsII@users.noreply.github.com>
2021-10-03 12:46:12 -0300

    Adicionei os Arquivos do Certificard
    Passando os códigos escritos do CodePen

---
+++ script.js
@@ -0,0 +1,5 @@
+function mudaTema() {
+  // Alterando o tema por meio da ALTERNâNCIA(toggle) entre a ativação dos temas (o comum e o DARK)
+  //Inclui a tag dark no body ao apertar o botão
+  document.body.classList.toggle("dark");
+}
